53b59991e270663a23514e8e47dede4ee1b4155a
Shamzic <shamery@free.fr>
2020-04-01 16:50:20 +0200

    screens and font

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './screens/home'
+import * as Font  from 'expo-font'
+import { AppLoading } from 'expo';
+
+const getFonts = () => Font.loadAsync({
+  'munito-regular' : require('./assets/fonts/Nunito-Regular.ttf'),
+  'munito-bold' : require('./assets/fonts/Nunito-Bold.ttf'),
+});
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if(fontLoaded) {
+    return (
+      <Home/>
+    );
+  } else {
+    return (
+      <AppLoading
+        startAsync={getFonts}
+        onFinish={() => setFontLoaded(true)}
+      />
+    )
+  }
+
 }
 
 const styles = StyleSheet.create({
